1af611cd472c06ad6818d3eb7f9bcb9327e95abc
Anatolii <anatolii.kryvda@gmail.com>
2021-04-19 02:49:05 +0300

    ServletConfig init parameters (not found ability to set)

---
+++ 9-servlet-config-init-parameters/src/main/java/demo/DemoApplication9.java
@@ -0,0 +1,32 @@
+package demo;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication9 {
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        return new TomcatServletWebServerFactory() {
+            @Override
+            protected TomcatWebServer getTomcatWebServer(Tomcat tomcat) {
+                // TODO: is there a way to set ServletConfig init parameters here or below?
+                return super.getTomcatWebServer(tomcat);
+            }
+
+            @Override
+            protected void postProcessContext(Context context) {
+                // context.addParameter("demo.value", "9");
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication9.class, args);
+    }
+}
