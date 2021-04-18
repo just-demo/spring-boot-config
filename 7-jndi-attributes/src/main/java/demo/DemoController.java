04e8b50c2d98ff2a417f0a055ffb399f99a1d638
Anatolii <anatolii.kryvda@gmail.com>
2021-04-19 01:40:27 +0300

    JNDI attributes (injection not working)

---
+++ 7-jndi-attributes/src/main/java/demo/DemoController.java
@@ -0,0 +1,30 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
+
+@RestController
+public class DemoController {
+    @Value("${demo.value:}")
+    private String value;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping
+    public Map<String, String> index() throws NamingException {
+        String value = (String) new InitialContext().lookup("java:comp/env/demo.value");
+        // TODO: should it have been evaluated with ${demo.value} ?
+        return singletonMap("value", value + " - " + this.value);
+    }
+}
