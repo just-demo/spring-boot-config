01d7faa3a21358b58e3f66661fe3e02dcd8a3350
Anatolii <anatolii.kryvda@gmail.com>
2021-04-21 01:26:47 +0300

    JDBC config server

---
+++ config-server-jdbc/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values ('config-client', 'default', 'master', 'demo.value', 'DEFAULT');
+
+insert into PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values ('config-client', 'dev', 'master', 'demo.value', 'DEV');
