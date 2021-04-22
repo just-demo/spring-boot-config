92f8ff3ca246587b5695bd812869ead6c02e854b
Anatolii <anatolii.kryvda@gmail.com>
2021-04-22 23:34:14 +0300

    Composite config server

---
+++ config-server-composite/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into PROPERTIES (APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values ('config-client', 'dev', 'master', 'demo.value', 'JDBC-DEV');
---
+++ config-server-composite/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table PROPERTIES (
+  APPLICATION varchar(100),
+  PROFILE varchar(100),
+  LABEL varchar(100),
+  KEY varchar(100),
+  VALUE varchar(100)
+);
