dc8176701603af5b639a1e7c43f329f19c1d7ac8
Anatolii <anatolii.kryvda@gmail.com>
2021-04-29 00:23:35 +0300

    Vault config server: cleanup

--- backends/vault/init.sh
+++ backends/vault/init.sh
@@ -1,2 +1,2 @@
-vault kv put secret/config-client demo.value=REDIS-DEFAULT
-vault kv put secret/config-client,dev demo.value=REDIS-DEV
+vault kv put secret/config-client demo.value=VAULT-DEFAULT
+vault kv put secret/config-client,dev demo.value=VAULT-DEV
